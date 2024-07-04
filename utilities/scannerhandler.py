0a488f8195e407c8476aedfb51b843b6448e2676
riboojo <ricardo.bqz@gmail.com>
2024-07-04 17:41:35 -0600

    Add report generation when oscap execution
    Add --report argument to generate html reports
    Temporally using ssg-firefox-xccdf as runs faster

--- utilities/scannerhandler.py
+++ utilities/scannerhandler.py
@@ -4,7 +4,7 @@ import subprocess
 class OscapScanner(object):
     def performScan(self):
         current_time = datetime.datetime.now()
-        report_filename = f'reports/{current_time}.xml'
+        result_filename = f'reports/{current_time}.xml'
+        report_filename = f'reports/{current_time}.html'
 
-        subprocess.run(['oscap', 'xccdf', 'eval', '--profile', 'xccdf_org.ssgproject.content_profile_stig', '--results', report_filename, '/usr/share/xml/scap/ssg/content/ssg-firefox-xccdf.xml'])
-
+        subprocess.run(['oscap', 'xccdf', 'eval', '--profile', 'xccdf_org.ssgproject.content_profile_stig', '--results', result_filename, '--report', report_filename, '/usr/share/xml/scap/ssg/content/ssg-firefox-xccdf.xml'])
