885a43a460f0347b98a9d552b11f0e481c364a63
riboojo <ricardo.bqz@gmail.com>
2024-07-04 21:35:24 -0600

    Move all arguments handing into ArgumentsHandler
    Small refactor to avoid spaguetti code and maintain consistency on classes functionalities

--- utilities/argumentshandler.py
+++ utilities/argumentshandler.py
@@ -1,14 +1,40 @@
 from utilities.scannerhandler import OscapScanner
+import argparse
 
 class OscapArguments(object):
+    def __init__(self):
+        self.scanner = OscapScanner()
+
+    def start(self):
+        parser = argparse.ArgumentParser(description='Simple command line tool for regular openscap scans')
+
+        parser.add_argument('command', type=str, nargs='?', default=None, help='Select one of the following options: scan, history, consult, compare')
+        parser.add_argument('id_consult', type=str, nargs='?', default=None, help='Select a valid ID (you can execute -history command to retrieve valid IDs)')
+        parser.add_argument('id_compare', type=str, nargs='?', default=None, help='Select a valid ID (you can execute -history command to retrieve valid IDs)')
+
+        args = parser.parse_args()
+        command = args.command
+        id_consult = args.id_consult
+        id_compare = args.id_compare
+
+        if not command:
+            self.printMenu()
+        elif command=='consult' and not id_consult:
+            self.printMenuConsult()
+        elif command=='compare' and not id_compare:
+            self.printMenuCompare()
+        else:
+            scanner = OscapScanner()
+            scanner.executeFeature(command, id_consult, id_compare)
+
     def printMenu(self):
         print(f"Select one of the following commands:\n1: scan\n2: history\n3: consult\n4: compare")
         selection = input("> ")
 
         if selection == '1':
-            self.executeFeature('scan')
+            self.scanner.executeFeature('scan')
         elif selection == '2':
-            self.executeFeature('history')
+            self.scanner.executeFeature('history')
         elif selection == '3':
             self.printMenuConsult()
         elif selection == '4':
@@ -19,24 +45,11 @@ class OscapArguments(object):
     def printMenuConsult(self):
         print(f"Enter an ID to consult its report")
         id_consult = input("> ")
-        self.executeFeature('consult', id_consult)
+        self.scanner.executeFeature('consult', id_consult)
 
     def printMenuCompare(self):
         print(f"Enter the first ID to compare")
         id_consult = input("> ")
         print(f"Enter the second ID to compare")
         id_compare = input("> ")
-        self.executeFeature('compare', id_consult, id_compare)
-
-    def executeFeature(self, command, id_consult=None, id_compare=None):
-        if command == 'scan':
-            scanner = OscapScanner()
-            scanner.performScan()
-        elif command == 'history':
-            print(f"TODO: print history")
-        elif command == 'consult':
-            print(f"TODO: print report for #{id_consult}")
-        elif command == 'compare':
-            print(f"TODO: print comparison between #{id_consult} and #{id_compare}")
-        else:
-            print(f"{command} is not recognized as a valid command")
+        self.scanner.executeFeature('compare', id_consult, id_compare)
--- utilities/scannerhandler.py
+++ utilities/scannerhandler.py
@@ -8,3 +8,15 @@ class OscapScanner(object):
         report_filename = f'reports/{current_time}.html'
 
         subprocess.run(['oscap', 'xccdf', 'eval', '--profile', 'xccdf_org.ssgproject.content_profile_stig', '--results', result_filename, '--report', report_filename, '/usr/share/xml/scap/ssg/content/ssg-firefox-xccdf.xml'])
+
+    def executeFeature(self, command, id_consult=None, id_compare=None):
+        if command == 'scan':
+            self.performScan()
+        elif command == 'history':
+            print(f"TODO: print history")
+        elif command == 'consult':
+            print(f"TODO: print report for #{id_consult}")
+        elif command == 'compare':
+            print(f"TODO: print comparison between #{id_consult} and #{id_compare}")
+        else:
+            print(f"{command} is not recognized as a valid command")
