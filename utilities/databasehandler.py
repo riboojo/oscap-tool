40c8d645f45dc880052eed8bb90e0ed5f9db85eb
riboojo <ricardo.bqz@gmail.com>
2024-07-05 09:14:19 -0600

    Add history print functionality
    Create a DatabaseHandler to save and restore scan's data
    Run an INSERT query into scans table on every scan command
    Run a SELECT query from scans table on every history command

---
+++ utilities/databasehandler.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class OscapDatabase(object):
+    def __init__(self, db_name=f'database/oscap_scans_history.db'):
+        self.dbname = db_name
+
+    def open(self):
+        self.connection = sqlite3.connect(self.dbname)
+        self.cursor = self.connection.cursor()
+        self.createTable()
+
+    def createTable(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS scans (
+                scan_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                result_path TEXT NOT NULL,
+                report_path TEXT NOT NULL
+            )
+        ''')
+        self.connection.commit()
+
+    def addScan(self, timestamp, report_path, result_path):
+        self.cursor.execute('INSERT INTO scans (timestamp, result_path, report_path) VALUES (?, ?, ?)', (timestamp, result_path, report_path))
+        self.connection.commit()
+
+    def getScans(self):
+        self.cursor.execute('SELECT scan_id, timestamp FROM scans')
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.connection.close()
--- utilities/scannerhandler.py
+++ utilities/scannerhandler.py
@@ -1,7 +1,11 @@
 import datetime
 import subprocess
+from utilities.databasehandler import OscapDatabase
 
 class OscapScanner(object):
+    def __init__(self):
+        self.db = OscapDatabase()
+
     def performScan(self):
         current_time = datetime.datetime.now()
         result_filename = f'reports/{current_time}.xml'
@@ -9,11 +13,23 @@ class OscapScanner(object):
 
         subprocess.run(['oscap', 'xccdf', 'eval', '--profile', 'xccdf_org.ssgproject.content_profile_stig', '--results', result_filename, '--report', report_filename, '/usr/share/xml/scap/ssg/content/ssg-firefox-xccdf.xml'])
 
+        self.db.open()
+        self.db.addScan(current_time, result_filename, report_filename)
+        self.db.close()
+
+    def readHistory(self):
+        self.db.open()
+        allScans = self.db.getScans()
+        self.db.close()
+
+        for scan_id, timestamp in allScans:
+            print(f'ID #{scan_id} generated on {timestamp}')
+
     def executeFeature(self, command, id_consult=None, id_compare=None):
         if command == 'scan':
             self.performScan()
         elif command == 'history':
-            print(f"TODO: print history")
+            self.readHistory()
         elif command == 'consult':
             print(f"TODO: print report for #{id_consult}")
         elif command == 'compare':
